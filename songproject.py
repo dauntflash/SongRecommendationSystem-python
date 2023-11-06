19d1786e4bb9ff2dd703e111745056df8b703ba4
dauntflash <107225248+dauntflash@users.noreply.github.com>
2023-11-06 16:44:27 +0300

    Update of the recommendation system.
    First update of the recommendation system.

--- songproject.py
+++ songproject.py
@@ -1,11 +1,20 @@
 import json
+import os #initiates the os library which will be used later on to open the playlists directories
 import random;
 
-tracks=["trap","Hiphop","reggae","Mugithi", "RnBs","genge","dancehall","bongo","drill kenya","drill"]
-while True:
-    songName = input("\n\nEnter the song or artist name(Enter q to quit): ").lower()
+folder_name="Playlists"   #You make sure you playlists folder is named 'Playlists'
+
+tracks=os.listdir(folder_name) # This command now reads the directory of the folder_name which is 'Paylists'. So your folder name for playlists must be saved Playlists.
+
+while True:  #initiating a loop which will make the program ask for input unless one decides to quit.
+
+    songName = input("\n\nEnter the song or artist name (Enter q to quit): ").lower()      #When one enters 'q' as input, we will terminate the while loop initiated above.
     def readfile(file):
-        file_path ="Playlists/"+file+".json"
+        file_path =folder_name+"/"+file+".json"
+
+        if(not os.path.isfile(file_path)):   #If the directory is not found return false
+           return False
+        
         with open(file_path) as f:
             data = json.load(f) # Load the JSON data from the file into a Python dictionary
             
@@ -30,14 +39,17 @@ while True:
                     return True
             else:
                 return False
-    if songName=="q":
-        print("Thank you for using our recommendation system.... ")
-        break
+            
+
+    if songName=="q":  #Here we now implement the loop termination. If one inserts q as input, we break. That means that the program stops running.
+        print("Thank you for using our recommendation system.... ")     #This is the message the user gets
+        break                                                       #The program is then terminated
             
 
     checker = False
     for track in tracks:
-        if(readfile(track)):
+        track_name= str.split(track, ".")#Here we split the track name from the .json part. To prevent giving an output like "Bongo.json alternatives" but just "Bongo alternatives"
+        if(readfile(track_name[0]) and track_name[1] == "json"):  #checks if the file in the folder has a ".json" part. In short checking if it is json file.
             checker = True
             break
 
